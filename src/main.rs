c2c9329d33f57affb0378650a74614182771c225
Alexey Seledtsov <rustpg@yandex.ru>
2017-05-09 17:06:00 +0300

    Добавлено:   - обработка параметров командной строки(crate clap)

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+extern crate clap;
+
+use clap::{App,AppSettings,SubCommand,Arg};
+
+fn main() {
+    let menu = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .setting(AppSettings::GlobalVersion)
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(SubCommand::with_name("run")
+            .about("Run server api")
+            .arg(Arg::with_name("config")
+                 .short("c")
+                 .long("config")
+                 .help("Path to config for server api")
+                 .takes_value(true)))
+        .get_matches();
+
+    match menu.subcommand() {
+        ("run", Some(cmd)) => {
+            // todo
+        },
+        _ => {
+            println!("{}", menu.usage());
+        }
+    }
+}
